b7b9b342983a9f9c9bc36ead18f59d36d739a315
armvishal <vishal.gill@armentum.co>
2021-05-13 00:02:25 +0530

    changes with file upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,15 +5,22 @@ import gql from "graphql-tag";
 const GET_BOOKS = gql`
   query fetchBooksDetails {
     getBooksList {
-      title,
+      title
       description
+    }
+  }
+`;
+const FILE_UPLOAD = gql`
+  mutation upload($file: Upload!, $name: String) {
+    upload(file: $file, name: $name) {
+      message
     }
   }
 `;
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
   books: any[];
@@ -27,16 +34,19 @@ export class HomeComponent implements OnInit {
       .query<any>({
         query: GET_BOOKS,
         context: {
-          headers: { 'token': "123465798jhgjhgsajhja321654", 'Cache-Control': 'no-cache' }
-        }
+          headers: {
+            token: "123465798jhgjhgsajhja321654",
+            "Cache-Control": "no-cache",
+          },
+        },
       })
       .subscribe(
         ({ data, loading }) => {
-          console.log(data)
+          console.log(data);
           this.books = data && data.getBooksList;
           this.loading = loading;
         },
-        error => {
+        (error) => {
           this.loading = false;
           this.error = error;
         }
@@ -47,5 +57,28 @@ export class HomeComponent implements OnInit {
     if (authors && authors.length > 1)
       return authors.reduce((acc, cur) => acc.name + ", " + cur.name);
     else return "";
+  }
+  upload($event) {
+    console.log($event.target.files[0]);
+    this.apollo
+      .mutate<any>({
+        mutation: FILE_UPLOAD,
+        variables: {
+          file: $event.target.files[0],
+          name: $event.target.files[0].name,
+        },
+        context: {
+          useMultipart: true,
+        },
+      })
+      .subscribe(
+        ({ data }) => {
+          console.log(data);
+        },
+        (error) => {
+          this.loading = false;
+          this.error = error;
+        }
+      );
   }
 }
